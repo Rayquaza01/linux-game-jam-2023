ad5480d8f8ad98a8778f20e8860d96d06fdcf4d3
Joe Jarvis <joe.jarvis109@gmail.com>
2023-06-01 12:54:49 -0400

    Player picks up exp, exp follows player

--- linux-game-jam-2023/Assets/Scripts/Experience.cs
+++ linux-game-jam-2023/Assets/Scripts/Experience.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Experience : MonoBehaviour {
     public float exp = 1f;
+    public float speed = 5f;
+    public bool followPlayer = false;
+
+    public string playerTag = "Player";
+    GameObject player;
 
     // Start is called before the first frame update
     void Start() {
-
+        player = GameObject.FindWithTag(playerTag);
     }
 
     // Update is called once per frame
     void Update() {
+        if (followPlayer) {
+            Vector2 heading = player.transform.position - transform.position;
+            Vector2 direction = heading / heading.magnitude;
 
+            transform.position += new Vector3(direction.x, direction.y) * speed * Time.deltaTime;
+        }
     }
 
     public void SetExperience(float e) {
--- linux-game-jam-2023/Assets/Scripts/Player.cs
+++ linux-game-jam-2023/Assets/Scripts/Player.cs
@@ -8,6 +8,8 @@ public class Player : MonoBehaviour {
 
     public float health = 100f;
 
+    public float experience = 0f;
+
     // Start is called before the first frame update
     void Start() {
         rb = GetComponent<Rigidbody2D>();
@@ -27,8 +29,19 @@ public class Player : MonoBehaviour {
         if (Input.GetKey(KeyCode.A)) {
             transform.position += -transform.right * speed * Time.deltaTime;
         }
+    }
 
-        Debug.Log("Health: " + health);
+    void OnTriggerEnter2D(Collider2D c) {
+        if (c.gameObject.CompareTag("Experience")) {
+            c.gameObject.GetComponent<Experience>().followPlayer = true;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D c) {
+        if (c.gameObject.CompareTag("Experience")) {
+            experience += c.gameObject.GetComponent<Experience>().exp;
+            Destroy(c.gameObject);
+        }
     }
 
     void OnCollisionStay2D(Collision2D c) {
