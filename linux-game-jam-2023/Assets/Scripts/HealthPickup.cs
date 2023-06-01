1d0dc6fc78f18126202dfdba6accbc4d06b2858f
Joe Jarvis <joe.jarvis109@gmail.com>
2023-06-01 14:08:23 -0400

    Add Health Pickup

---
+++ linux-game-jam-2023/Assets/Scripts/HealthPickup.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPickup : MonoBehaviour {
+    public float heal = 10f;
+
+    public string playerTag = "Player";
+    GameObject player;
+    Player playerObj;
+
+    // Start is called before the first frame update
+    void Start() {
+        player = GameObject.FindWithTag(playerTag);
+        playerObj = player.GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
+
+    void OnCollisionEnter2D(Collision2D c) {
+        if (c.gameObject.CompareTag(playerTag)) {
+            playerObj.HealPlayer(heal);
+
+            Destroy(this.gameObject);
+        }
+    }
+
+}
