59ab30ff983afc9215442de6677235c1a6a29f01
Joe Jarvis <joe.jarvis109@gmail.com>
2023-06-01 13:37:43 -0400

    Toggleable spawner

--- linux-game-jam-2023/Assets/Scripts/Spawner.cs
+++ linux-game-jam-2023/Assets/Scripts/Spawner.cs
@@ -8,6 +8,8 @@ public class Spawner : MonoBehaviour {
     // number of seconds to spawn 1 enemy
     public float spawnFreq = 1;
 
+    public bool active = true;
+
     float currentTime = 0f;
 
     // Start is called before the first frame update
@@ -16,6 +18,8 @@ public class Spawner : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
+        if (!active) return;
+
         currentTime += Time.deltaTime;
         if (currentTime >= spawnFreq) {
             Instantiate(toSpawn, transform.position, transform.rotation);
