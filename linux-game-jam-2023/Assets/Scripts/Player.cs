c3159360cf03a8abce823bf844a5e063cfae805d
Joe Jarvis <joe.jarvis109@gmail.com>
2023-06-01 10:45:34 -0400

    Add basic player movement

--- linux-game-jam-2023/Assets/Scripts/Player.cs
+++ linux-game-jam-2023/Assets/Scripts/Player.cs
@@ -3,13 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
+    public float speed = 5f;
+    Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start() {
-
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update() {
-
+        if (Input.GetKey(KeyCode.W)) {
+            transform.position += transform.up * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            transform.position += -transform.up * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            transform.position += transform.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            transform.position += -transform.right * speed * Time.deltaTime;
+        }
     }
 }
